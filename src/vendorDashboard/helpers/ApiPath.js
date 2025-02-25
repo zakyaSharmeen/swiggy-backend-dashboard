31fe86614b282905627fd5bc2310c503c15657ec
zakya sharmeen <sakarim9124@gmail.com>
2025-02-25 21:46:36 +0530

    backend-dashboard

--- src/vendorDashboard/helpers/ApiPath.js
+++ src/vendorDashboard/helpers/ApiPath.js
@@ -1,1 +1,5 @@
-   export const API_Path = "http://localhost:4000";
+   // export const API_Path = "http://localhost:4000";
+
+
+   export const API_Path = "https://swiggy-backend-a2f4.onrender.com";
+
