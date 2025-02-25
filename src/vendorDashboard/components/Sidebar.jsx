c08c4be5957276bb95c44a9d68382b07428cc648
zakya sharmeen <sakarim9124@gmail.com>
2025-02-25 21:57:11 +0530

    backend-dashboard

--- src/vendorDashboard/components/Sidebar.jsx
+++ src/vendorDashboard/components/Sidebar.jsx
@@ -11,7 +11,6 @@ function Sidebar({
           {showFirmTitle ? <li onClick={showFirmHandler}>Add Firm</li> : ""}
           <li onClick={showProductHandler}>Add Product</li>
           <li onClick={showAllProductsHandler}>All Products</li>
-          <li>User Details</li>
         </ul>
       </div>
     </div>
